b8e734b0f1a23848831703bd73c8508a0c4a8239
Daniele21 <daniele21@users.noreply.github.com>
2026-08-29 09:43:22 +0200

    Apply Prettier output to overview

--- frontend/src/pages/overview/OverviewPage.tsx
+++ frontend/src/pages/overview/OverviewPage.tsx
@@ -280,11 +280,5 @@ export function OverviewPage({ onFindBestSetup, onTestModel }: OverviewPageProps
     );
   }
 
-  return (
-    <OverviewView
-      {...state.data}
-      onFindBestSetup={onFindBestSetup}
-      onTestModel={onTestModel}
-    />
-  );
+  return <OverviewView {...state.data} onFindBestSetup={onFindBestSetup} onTestModel={onTestModel} />;
 }
